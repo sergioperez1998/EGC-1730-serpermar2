8ff9c2cd354b98c7b37e4a399a9ac21136cebfe7
sergioperez1998 <sergio-pm@hotmail.com>
2020-09-05 13:44:13 +0200

    Realizando los cambios necesarios para desplegar la aplicacion

---
+++ carmela/bin/django-admin.py
@@ -0,0 +1,5 @@
+#!/home/jesus/Desktop/EGC/Ejercicios_de_examen/EGC-1730/EGC-1730-serpermar2/carmela/bin/python3
+from django.core import management
+
+if __name__ == "__main__":
+    management.execute_from_command_line()
